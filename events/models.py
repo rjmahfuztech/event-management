588278f106645805ef055648696ab088580375cf
rjmahfuztech <rjmahfuztech@gmail.com>
2025-01-23 10:00:59 +0600

    Category, Event and Participant added in model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=300)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        default="Corporate Events",
+        related_name='event'
+    )
+
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    event = models.ManyToManyField(Event, related_name='participant')
