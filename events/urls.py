48dfca285c360551b7bdf2a310f62137dbc45659
rjmahfuztech <rjmahfuztech@gmail.com>
2025-01-26 00:08:08 +0600

    CRUD operation, Filter, dynamic dashboard, often all done

--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
-from events.views import events,event_details,dashboard,add_event,add_participant,add_category
+from events.views import events_info,event_details,dashboard,add_event,add_participant,add_category,category,participant,event,update_event,delete_event,update_participant,delete_participant,update_category,delete_category
+
 
 urlpatterns = [
-    path('event/', events, name='event'),
+    path('event-info/', events_info, name='event-info'),
     path('event-details/<int:id>', event_details, name='details'),
     path('dashboard/', dashboard, name='dashboard'),
-    path('add-event/', add_event, name='add-event'),
-    path('add-participant/', add_participant, name='add-participant'),
+    path('category/', category, name='category'),
     path('add-category/', add_category, name='add-category'),
+    path('update-category/<int:id>', update_category, name='update-category'),
+    path('delete-category/<int:id>', delete_category, name='delete-category'),
+    path('event/', event, name='event'),
+    path('add-event/', add_event, name='add-event'),
+    path('update-event/<int:id>', update_event, name='update-event'),
+    path('delete-event/<int:id>', delete_event, name='delete-event'),
+    path('participant/', participant, name='participant'),
+    path('add-participant/', add_participant, name='add-participant'),
+    path('update-participant/<int:id>', update_participant, name='update-participant'),
+    path('delete-participant/<int:id>', delete_participant, name='delete-participant'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,30 +1,89 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect
 from events.forms import EventModelForm,ParticipantModelForm,CategoryModelForm
 from events.models import Event,Category,Participant
 from django.contrib import messages
+from datetime import date
+from django.db.models import Count,Q
 
+
+## Common Query
+prefetch_query = Event.objects.prefetch_related('participant')
+select_query = Event.objects.select_related('category')
 # Create your views here.
-def events(request):
-    event_data = Event.objects.all()
+def events_info(request):
+    event_query = prefetch_query
+    categories = Category.objects.all()
+    # filter data
+    search_name = request.GET.get('name', 'all')
+    choice = request.GET.get('category', 'all')
+    start_date = request.GET.get('start-date', 'all')
+    end_date = request.GET.get('end-date', 'all')
+
+
+    if search_name != 'all' and choice != 'all':
+        event_data = event_query.filter(name__icontains=search_name, category=choice)
+    elif start_date != 'all' and end_date != 'all':
+        event_data = event_query.filter(date__gte=start_date, date__lte=end_date)
+    elif search_name != 'all':
+        event_data = event_query.filter(name__icontains=search_name)
+    elif choice != 'all':
+        event_data = event_query.filter(category=choice)
+    else:
+        event_data = event_query.all()
+
     context ={
-        "event_data": event_data
+        "event_data": event_data,
+        "categories": categories
     }
-    return render(request, "events.html",context)
+    return render(request, "event_info.html",context)
 
 def event_details(request, id):
-    details = Event.objects.get(id=id)
+    details = prefetch_query.get(id=id)
     context ={
         "details": details
     }
     return render(request, "event_details.html",context)
 
 def dashboard(request):
-    events = Event.objects.all()
+    events_query = select_query.all()
+    type = request.GET.get('type', 'all')
+
+    # filtering by date
+    todays_event = events_query.filter(date=date.today())
+    
+    if type == 'upcoming':
+        events = events_query.filter(date__gt=date.today())
+    elif type == 'past':
+        events = events_query.filter(date__lt=date.today())
+    else:
+        events = events_query.all()
+
+
+    # counting
+    total_participant = Participant.objects.all().count()
+    count = Event.objects.aggregate(
+        total_event=Count('id'),
+        total_upcoming=Count('id', filter=Q(date__gt=date.today())),
+        total_past=Count('id', filter=Q(date__lt=date.today())),
+    )
     context ={
-        "events": events
+        "events": events,
+        "todays_event": todays_event,
+        "count": count,
+        "total_participant": total_participant
     }
+    
     return render(request, "dashboard/dashboard.html", context)
 
+# get event
+def event(request):
+    events = select_query.all()
+    context = {
+        'events': events 
+    }
+    return render(request, "event.html", context)
+
+# Event Operation
 def add_event(request):
     event_form = EventModelForm()
 
@@ -40,6 +99,40 @@ def add_event(request):
     }
     return render(request, "form.html", context)
 
+def update_event(request,id):
+    event = Event.objects.get(id=id)
+    event_form = EventModelForm(instance=event)
+    
+    if request.method == "POST":
+        event_form = EventModelForm(request.POST, instance=event)
+
+        if event_form.is_valid():
+            event_form.save()
+            messages.success(request, 'Event Updated Successful!')
+            return redirect('update-event', id)
+
+    context = {'event_form': event_form}
+    return render(request, "form.html", context)
+
+def delete_event(request, id):
+    event = Event.objects.get(id=id)
+    if request.method == "POST":
+        event.delete()
+        messages.success(request, 'Event Deleted Successful!')
+        return redirect('event')
+    else:
+        messages.error(request, 'Opps! Something Went Wrong! Event Delete Failed!')
+        return redirect('event')
+
+# get participant
+def participant(request):
+    participants = Participant.objects.all()
+    context={
+        'participants': participants    
+    }
+    return render(request, "participant.html", context)
+
+# Participant Operation
 def add_participant(request):
     participant_form = ParticipantModelForm()
 
@@ -47,7 +140,7 @@ def add_participant(request):
         participant_form = ParticipantModelForm(request.POST)
         if participant_form.is_valid():
             participant_form.save()
-            messages.success(request, 'One Participant Added Successful!')
+            messages.success(request, 'Participant Added Successful!')
             return redirect('add-participant')
 
     context = {
@@ -55,6 +148,40 @@ def add_participant(request):
     }
     return render(request, "form.html", context)
 
+def update_participant(request,id):
+    participant = Participant.objects.get(id=id)
+    participant_form = ParticipantModelForm(instance=participant)
+
+    if request.method == "POST":
+        participant_form = ParticipantModelForm(request.POST, instance=participant)
+
+        if participant_form.is_valid():
+            participant_form.save()
+            messages.success(request, "Participant Updated Successful!")
+            return redirect('update-participant', id)
+    
+    context = {'participant_form': participant_form}
+    return render(request, "form.html", context)
+
+def delete_participant(request,id):
+    participant = Participant.objects.get(id=id)
+    if request.method == "POST":
+        participant.delete()
+        messages.success(request, "Participant Updated Successful!")
+        return redirect('participant')
+    else:
+        messages.error(request, 'Opps! Something Went Wrong! Participant Delete Failed!')
+        return redirect('participant')
+
+# get category
+def category(request):
+    categories = Category.objects.all()
+    context={
+        'categories': categories    
+    }
+    return render(request, "category.html", context)
+
+# Category Operation
 def add_category(request):
     category_form = CategoryModelForm()
 
@@ -69,3 +196,29 @@ def add_category(request):
         "category_form": category_form
     }
     return render(request, "form.html", context)
+
+def update_category(request,id):
+    category = Category.objects.get(id=id)
+    category_form = CategoryModelForm(instance=category)
+
+    if request.method == "POST":
+        category_form = CategoryModelForm(request.POST, instance=category)
+
+        if category_form.is_valid():
+            category_form.save()
+            messages.success(request, "Category Updated Successful!")
+            return redirect('update-category', id)
+        
+    context = {"category_form": category_form}
+    return render(request, "form.html", context)
+
+def delete_category(request,id):
+    category = Category.objects.get(id=id)
+
+    if request.method == "POST":
+        category.delete()
+        messages.success(request, "Category Deleted Successful!")
+        return redirect('category')
+    else:
+        messages.success(request, 'Opps! Something Went Wrong! Event Delete Failed!')
+        return redirect('category')
