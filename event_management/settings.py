1e5117bd9e15d8b2a4548806b5ba03da246c64d3
rjmahfuztech <rjmahfuztech@gmail.com>
2025-07-23 14:56:51 +0600

    Prepare for Railway deploy

--- event_management/settings.py
+++ event_management/settings.py
@@ -78,11 +78,16 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# PostgreSQL
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
